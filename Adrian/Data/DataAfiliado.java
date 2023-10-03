8b90ca4650b223e8853d3ebbe2f3f0752971b665
AdrianCabello252 <cabelloadrian75@gmail.com>
2023-10-03 16:21:31 -0300

    Clases Data

---
+++ Adrian/Data/DataAfiliado.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+/**
+ *
+ * @author marce
+ */
+public class DataAfiliado {
+    private int idAfiliado;
+    private String nombre;
+    private String apellido;
+    private int dni;
+    private boolean activo;
+
+    public DataAfiliado(int idAfiliado, String nombre, String apellido, int dni, boolean activo) {
+        this.idAfiliado = idAfiliado;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.activo = activo;
+    }
+
+    public int getIdAfiliado() {
+        return idAfiliado;
+    }
+
+    public void setIdAfiliado(int idAfiliado) {
+        this.idAfiliado = idAfiliado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    
+}
---
+++ Adrian/Data/DataOrden.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author marce
+ */
+public class DataOrden {
+    private int idOrden;
+    private LocalDate fecha;
+    private String formaPago;
+    private double importe;
+    private DataAfiliado idAfiliado;
+    private DataPrestador prestador;
+
+    public DataOrden(int idOrden, LocalDate fecha, String formaPago, double importe, DataAfiliado idAfiliado, DataPrestador prestador) {
+        this.idOrden = idOrden;
+        this.fecha = fecha;
+        this.formaPago = formaPago;
+        this.importe = importe;
+        this.idAfiliado = idAfiliado;
+        this.prestador = prestador;
+    }
+
+    public int getIdOrden() {
+        return idOrden;
+    }
+
+    public void setIdOrden(int idOrden) {
+        this.idOrden = idOrden;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getFormaPago() {
+        return formaPago;
+    }
+
+    public void setFormaPago(String formaPago) {
+        this.formaPago = formaPago;
+    }
+
+    public double getImporte() {
+        return importe;
+    }
+
+    public void setImporte(double importe) {
+        this.importe = importe;
+    }
+
+    public DataAfiliado getIdAfiliado() {
+        return idAfiliado;
+    }
+
+    public void setIdAfiliado(DataAfiliado idAfiliado) {
+        this.idAfiliado = idAfiliado;
+    }
+
+    public DataPrestador getPrestador() {
+        return prestador;
+    }
+
+    public void setPrestador(DataPrestador prestador) {
+        this.prestador = prestador;
+    }
+    
+    
+}
